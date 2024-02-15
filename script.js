71381c07256e4ebb9561d867d2b86d0c31bf54d5
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-15 13:52:24 +0600

    total price validation finish

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ let count = 1;
 let total = 0;
 
 
+const couponCode = document.getElementById('apply-btn');
+couponCode.addEventListener('click', function() {
+    if( total <= 200) {
+        return alert('You buy the 200$ product');
+    }
+})
+
 
 for (let index = 0; index < cards.length; index++) {
     const element = cards[index];
@@ -21,7 +28,29 @@ for (let index = 0; index < cards.length; index++) {
 
 
 
+        if( total >= 200) {
 
+            const couponCode = document.getElementById('apply-btn');
+            couponCode.addEventListener('click', function() {
+
+                const inputValue = document.getElementById('input-field').value.toUpperCase();
+                const input = inputValue.split(' ').join('');
+
+                if( input === "SELL200"){
+                    const discountValue = document.getElementById('discountPrice');
+                    const grandTotal = document.getElementById('total');
+                    const discount = Number((total * 0.2).toFixed(2));
+                    discountValue.innerText = discount;
+                    const currentTotal = total - discount;
+                    grandTotal.innerText = currentTotal;
+
+                } else {
+                    alert('Coupon Code Not Match !');
+                }
+            })
+
+
+        }
         
         const productList = document.getElementById('title-container');
         const p = document.createElement('p');
