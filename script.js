d88b4f0a1cece6a77a0fbe8394298d04c37ab78e
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-15 13:30:24 +0600

    discount calculation done

--- script.js
+++ script.js
@@ -22,28 +22,6 @@ for (let index = 0; index < cards.length; index++) {
 
 
 
-        if( total >= 200) {
-
-            const couponCode = document.getElementById('apply-btn');
-            couponCode.addEventListener('click', function() {
-                const inputValue = document.getElementById('input-field').value.toUpperCase();
-                const input = inputValue.split(' ').join('');
-
-                if( input === "SELL200"){
-                    const discountValue = document.getElementById('discountPrice');
-                    const grandTotal = document.getElementById('total');
-                    const discount = total * 0.2;
-                    discountValue.innerText = discount;
-                    const currentTotal = total - discount;
-                    console.log(currentTotal);
-
-                } else {
-                    // console.log('coupon code not match')
-                }
-            })
-
-
-        }
         
         const productList = document.getElementById('title-container');
         const p = document.createElement('p');
